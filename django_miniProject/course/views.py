85c008eebf7c38e4a9ba80aa0d18c223cdf6c865
jjung0121 <45662450+jjung0121@users.noreply.github.com>
2019-03-13 11:24:15 +0900

    전공 전체 조회 기능 구현

--- django_miniProject/course/views.py
+++ django_miniProject/course/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from . models import Major
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 # Create your views here.
+
+major_list = ListView.as_view(model=Major)
