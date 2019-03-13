9d766dbd790c19ad0f5f655ba18b500fa3513b49
jjung0121 <45662450+jjung0121@users.noreply.github.com>
2019-03-13 12:35:14 +0900

    #3 major db 모델 수정

--- django_miniProject/course/models.py
+++ django_miniProject/course/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Major(models.Model):
-    major_id = models.CharField(max_length=50)
-    major_title = models.CharField(max_length=50)
+    major_id = models.IntegerField(primary_key=True, verbose_name="전공코드")
+    major_title = models.CharField(max_length=50, verbose_name="전공명")
 
     def __str__(self): # admin page에서 노출되는 값
         return self.major_title
